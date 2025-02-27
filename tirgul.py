cc3494d08ba3613b1878a401333da2ff17f606e3
Daniel146 <danielalimov146@gmail.com>
2025-02-27 14:41:22 +0200

    hola

--- tirgul.py
+++ tirgul.py
@@ -1,3 +1,4 @@
+
 user_input = input("Enter your name: ")
 print("Hi " + user_input + "!")
 
@@ -12,3 +13,15 @@ import datetime
 current_year = datetime.datetime.now().year
 year_of_birth = input("Enter your year of birth: ")
 print(f"{user_input} You are", current_year - int(year_of_birth), "years old")
+
+
+
+number = input("Enter a three digits number: ")
+if len(number) != 3:
+    print("The number is not three digits")
+else:
+    print("your last digit is ", number[-1])
+    print("your middle digit is ", number[1])
+    print("your first digit is ", number[0])
+    avarage =  (int(number[0]) + int(number[1]) + int(number[2])) / 3
+    print("The avarage of the digits is: ", "{:.3f}".format(avarage))
